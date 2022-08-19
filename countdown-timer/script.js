8b05576111f98b780b5cdfc8f266fcd79cdae8bd
Jeff Chavez <jeff.chavez0828@gmail.com>
2022-08-19 13:49:05 +0800

    Added make the format of seconds and minutes less than 10 two digits

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -1,9 +1,9 @@
-const newYears = "1 Jan 2023";
+const newYears = "10 June 2023";
 
 
 const countdown = () => {
     const newYearsDate = new Date(newYears);
-    const currentDate = new Date().getTime();
+    const currentDate = new Date();
     const totalSeconds = Math.floor((newYearsDate - currentDate) / 1000);
 
     const hours = Math.floor(totalSeconds / 3600) % 24;
@@ -13,14 +13,18 @@ const countdown = () => {
 
     document.getElementById("days").innerText = days;
     document.getElementById("hours").innerText = hours;
-    document.getElementById("minutes").innerText = minutes;
-    document.getElementById("seconds").innerText = seconds;
+    document.getElementById("minutes").innerText = formatTime(minutes);
+    document.getElementById("seconds").innerText = formatTime(seconds);
     console.log(days, hours, minutes, seconds)
+}
+
+// make the format of seconds and minutes less than 10 two digits
+const formatTime = (time) => {
+    return time < 10 ? (`0${time}`) : time; 
 }
 
 
 setInterval(countdown, 1000);
-
 
 
 countdown();
