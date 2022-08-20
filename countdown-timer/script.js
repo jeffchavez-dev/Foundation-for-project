8e37b9d8047ed6d28cdc9476c95d46e6652ba706
Jeff Chavez <jeff.chavez0828@gmail.com>
2022-08-20 11:22:54 +0800

    Added setDate controls

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -1,38 +1,50 @@
-const newYears = "10 June 2023";
+
 const myEvent = document.getElementById("myEvent");
+const myEventDate = document.getElementById("myEventDate");
 const enteredEvent = document.getElementById("enteredEvent");
-const btn = document.getElementById("btn");
-const controls = document.getElementById("controls");
+const setEventBtn = document.getElementById("setEventBtn");
+const setDateBtn = document.getElementById("setDateBtn");
+const setEvent = document.getElementById("setEvent");
+const control = document.getElementById("control-gear");
 
 
-controls.addEventListener('click', () => {
-    myEvent.classList.toggle("show");
-    btn.classList.toggle("show");
+control.addEventListener('click', () => {
+    setEvent.classList.toggle("show");
 })
 
-btn.addEventListener("click", () => {
+setEventBtn.addEventListener("click", () => {
     enteredEvent.innerHTML = myEvent.value;
     myEvent.setAttribute("value","New");
+})
+
+let newYears = "";
+setDateBtn.addEventListener("click", () => {
+    newYears = myEventDate.value;
 })
 
 
 
 
 const countdown = () => {
-    const newYearsDate = new Date(newYears);
-    const currentDate = new Date();
-    const totalSeconds = Math.floor((newYearsDate - currentDate) / 1000);
-
-    const hours = Math.floor(totalSeconds / 3600) % 24;
-    const days = Math.floor(totalSeconds / 3600 / 24);
-    const minutes = (Math.floor(totalSeconds / 60) % 24) % 60;
-    const seconds = Math.floor(totalSeconds) % 60;
-
-    document.getElementById("days").innerText = days;
-    document.getElementById("hours").innerText = formatTime(hours);
-    document.getElementById("minutes").innerText = formatTime(minutes);
-    document.getElementById("seconds").innerText = formatTime(seconds);
-    console.log(days, hours, minutes, seconds)
+    if (newYears == "") {  
+        return 0;
+    } else {
+        const newYearsDate = new Date(newYears);
+        const currentDate = new Date();
+        const totalSeconds = Math.floor((newYearsDate - currentDate) / 1000);
+    
+        const hours = Math.floor(totalSeconds / 3600) % 24;
+        const days = Math.floor(totalSeconds / 3600 / 24);
+        const minutes = (Math.floor(totalSeconds / 60) % 24) % 60;
+        const seconds = Math.floor(totalSeconds) % 60;
+    
+        document.getElementById("days").innerText = days;
+        document.getElementById("hours").innerText = formatTime(hours);
+        document.getElementById("minutes").innerText = formatTime(minutes);
+        document.getElementById("seconds").innerText = formatTime(seconds);
+        console.log(days, hours, minutes, seconds) 
+    }
+   
 }
 
 // make the format of seconds and minutes less than 10 two digits
