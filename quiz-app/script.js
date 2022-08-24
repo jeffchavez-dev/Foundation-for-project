a53c94a9d4c0314e66df8073c422437de7793ecc
Jeff Chavez <jeff.chavez0828@gmail.com>
2022-08-24 14:38:40 +0800

    updated label

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -32,17 +32,15 @@ const quizData = [
 
 //declare variables 
 const questionEl = document.getElementById("question");
-const a_text = document.getElementById("a");
-const b_text = document.getElementById("b");
-const c_text = document.getElementById("c");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
 const submitBtn = document.getElementById("btn");
 
-a_text.value = 'Okay';
-console.log(a_text);
 
 // Keep track of the current question - 0 Index
 let currentQuiz = 0;
-let answer = undefined;
+let score = 0; 
 
 // add loadQuiz function that will populate the UI
 // this will be called everytime we click submit
@@ -54,26 +52,40 @@ const loadQuiz = () => {
     c_text.innerText = currentQuizData.c;
 }
 
-
+// check if an answer is selected 
+// when submit is clicked, the selection must be deselected
+//
 const getSelected = () => {
+   
     const answerEls = document.querySelectorAll(".answer");
 
+    let answer = undefined;
+    
     answerEls.forEach((answerEl) => {
         if(answerEl.checked) {
             answer = answerEl.id;
         }
-    })
+    });
+
+    return answer;
 }
 
 
 submitBtn.addEventListener('click', () => {
-    currentQuiz++;
+  
+    // check to see the answer
+    const answer = getSelected();
 
-    if(currentQuiz < quizData.length) {
-        loadQuiz();
-    } else {
-        // TODO: Show Results
-        console.log("You've finished!");
+    if(answer) {
+        
+        currentQuiz++;
+        if(currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            // TODO: Show Results
+            console.log("You've finished!");
+        }
     }
+   
   
 })
