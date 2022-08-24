117b6f2395dac7e6b1f109ff8007c9737952a7b7
Jeff Chavez <jeff.chavez0828@gmail.com>
2022-08-24 15:37:00 +0800

    added start quiz button

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -4,7 +4,7 @@ const quizData = [
         a:'Sanctification',
         b: 'Justification',
         c: 'Faith',
-        corect: 'b'
+        correct: 'b'
     
     }, 
     {
@@ -12,39 +12,50 @@ const quizData = [
         a:'Incarnation',
         b: 'Resurrection',
         c: 'Cross',
-        corect: 'a'
+        correct: 'a'
     },
     {
         question: 'Who is the author of the Epistle to Hebrews',
         a:'Apollos',
         b: 'Silvanus',
         c: 'Paul',
-        corect: 'c'
+        correct: 'c'
     },
     {
         question: 'It is the power of God unto salvation for those who believe.',
         a:'Christ',
         b: 'Gospel',
         c: 'Grace',
-        corect: 'b'
+        correct: 'b'
     }
 ];
 
 //declare variables 
 const questionEl = document.getElementById("question");
+const startQuiz = document.getElementById("start-quiz");
+const quiz = document.getElementById("quiz");
 const a_text = document.getElementById("a_text");
 const b_text = document.getElementById("b_text");
 const c_text = document.getElementById("c_text");
 const submitBtn = document.getElementById("btn");
+const answerEls = document.querySelectorAll(".answer");
 
+
+startQuiz.addEventListener('click', () => {
+    quiz.classList.add('show');
+    console.log('Clicked')
+})
 
 // Keep track of the current question - 0 Index
 let currentQuiz = 0;
 let score = 0; 
 
+
+
 // add loadQuiz function that will populate the UI
 // this will be called everytime we click submit
 const loadQuiz = () => {
+    deSelectAnswers();
     const currentQuizData = quizData[currentQuiz];
     questionEl.innerText = currentQuizData.question;
     a_text.innerText = currentQuizData.a;
@@ -52,12 +63,11 @@ const loadQuiz = () => {
     c_text.innerText = currentQuizData.c;
 }
 
+loadQuiz();
 // check if an answer is selected 
 // when submit is clicked, the selection must be deselected
-//
+// 
 const getSelected = () => {
-   
-    const answerEls = document.querySelectorAll(".answer");
 
     let answer = undefined;
     
@@ -71,19 +81,29 @@ const getSelected = () => {
 }
 
 
+function deSelectAnswers(){
+    answerEls.forEach((answerEl) => {
+      answerEl.checked = false;
+    })
+}
+
 submitBtn.addEventListener('click', () => {
   
     // check to see the answer
     const answer = getSelected();
 
     if(answer) {
-        
+
+        if(answer === quizData[currentQuiz].correct) {
+            score++;
+        }
+
         currentQuiz++;
         if(currentQuiz < quizData.length) {
             loadQuiz();
         } else {
             // TODO: Show Results
-            console.log("You've finished!");
+            quiz.innerHTML = `<h2 class="final-score">You answered correctly at ${score}/${quizData.length}`;
         }
     }
    
