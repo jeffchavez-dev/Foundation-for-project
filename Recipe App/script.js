1790125f4d065b3de4e05fdb62aa6351bc900272
Jeff Chavez <jeff.chavez0828@gmail.com>
2022-08-30 21:24:00 +0800

    Script updated

--- Recipe App/script.js
+++ Recipe App/script.js
@@ -1,2 +1,9 @@
 //Get random meals
 //Get meals by search
+
+const searchBtn = document.getElementById("search");
+const searchInput = document.getElementById("search-input");
+
+searchBtn.addEventListener('click', () => {
+    searchInput.innerText = searchInput.value;
+})
