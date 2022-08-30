02908c735d3573e4dd24c400bc69816cfeb51d5f
Jeff Chavez <jeff.chavez0828@gmail.com>
2022-08-31 02:37:16 +0800

    Update script.js

--- Recipe App/script.js
+++ Recipe App/script.js
@@ -2,6 +2,7 @@
 //Get meals by search
 
 const searchBtn = document.getElementById("search");
+const mealBtn = document.getElementById("btn");
 const searchInput = document.getElementById("search-input");
 const header = document.querySelector(".header-title");
 
