1eca6fa7b34da2d42fbefdf2ff6c135741626bd4
Jeff Chavez <jeff.chavez0828@gmail.com>
2022-10-03 20:13:41 +0800

    Updated Script

--- notes_app/script.js
+++ notes_app/script.js
@@ -1,7 +1,10 @@
 const editBtn = document.querySelector('.edit')
 const deleteBtn = document.querySelector('.delete')
 const notesEl = document.querySelector('.notes')
+const main = document.querySelector('.main')
+const textArea = document.querySelector('textarea')
 
 editBtn.addEventListener('click', () => {
-    notesEl
+    main.classList.toggle('hidden')
+    textArea.classList.toggle('hidden')
 })
