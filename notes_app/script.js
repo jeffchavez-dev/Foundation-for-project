dd1cf5777828c46976ee3abeaaedbdf01ac503c2
Jeff Chavez <jeff.chavez0828@gmail.com>
2022-10-05 14:45:09 +0800

    added add button

--- notes_app/script.js
+++ notes_app/script.js
@@ -14,3 +14,15 @@ textArea.addEventListener('input', (e) => {
     main.innerHTML = marked(value);
 
 })
+
+
+// <div class="notes">
+// <div class="tools">
+//     <i class="fa-solid fa-pen-to-square edit"></i>
+//     <i class="fa-solid fa-trash-can delete"></i>
+//     <i class="fa-sharp fa-solid fa-xmark"></i>
+// </div>
+// <div class="main hidden"></div>
+// <textarea placeholder="Type your notes here"></textarea>
+
+// </div>
